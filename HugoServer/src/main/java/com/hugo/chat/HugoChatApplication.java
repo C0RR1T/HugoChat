19f06d44b6afd988d11f3cf792a9eed0ffc2322d
L37 <a@b.c>
2021-04-07 10:57:34 +0200

    Added Spring boot

---
+++ HugoServer/src/main/java/com/hugo/chat/HugoChatApplication.java
@@ -0,0 +1,13 @@
+package com.hugo.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HugoChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HugoChatApplication.class, args);
+	}
+
+}
---
+++ HugoServer/src/test/java/com/hugo/chat/HugoChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HugoChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
