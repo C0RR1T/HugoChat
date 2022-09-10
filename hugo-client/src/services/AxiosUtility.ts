f680a580d3284b7a2a22dadda60d1f53ea2212fb
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-09-10 21:06:47 +0200

    new url uwu

--- hugo-client/src/services/AxiosUtility.ts
+++ hugo-client/src/services/AxiosUtility.ts
@@ -1,6 +1,6 @@
 import axios, {AxiosInstance} from "axios";
 
-const BASE_URL = "http://161.97.165.1:8080/api/v2";
+const BASE_URL = "https://hugo-chat.nilstrieb.dev/api/v2";
 //const BASE_URL = "http://localhost:8080";
 
 const defaultAxiosInstance: AxiosInstance = axios.create({
